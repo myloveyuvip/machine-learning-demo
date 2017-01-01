94f3f0f2b68bf25ac86b1707781c27834304a4e4
yuliyao <myloveyuvip@qq.com>
2017-01-01 18:54:26 +0800

    上传样本数据

---
+++ src/main/java/com/yuly/model/Sample.java
@@ -0,0 +1,9 @@
+package com.yuly.model;
+
+/**
+ * Created by yuliyao on 2017/1/1.
+ */
+public class Sample {
+
+
+}
---
+++ src/main/java/com/yuly/utils/ArffUtil.java
@@ -0,0 +1,11 @@
+package com.yuly.utils;
+
+/**
+ * Created by yuliyao on 2017/1/1.
+ */
+public class ArffUtil {
+
+    public void readArff() {
+
+    }
+}
